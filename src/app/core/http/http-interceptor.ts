2b60a81d3ed58873824954135f8a7f5f54e56774
fatemeh jahangiri <jahangiri.ir.it@gmail.com>
2023-06-06 17:36:05 +0430

    Feature: Add caching mechanisms to the interceptor to reduce API requests

--- src/app/core/http/http-interceptor.ts
+++ src/app/core/http/http-interceptor.ts
@@ -4,25 +4,37 @@ import {
   HttpEvent,
   HttpHandler,
   HttpInterceptor,
-  HttpRequest,
+  HttpRequest, HttpResponse,
 } from '@angular/common/http';
-import {catchError, Observable, throwError} from 'rxjs';
+import {catchError, Observable, of, tap, throwError} from 'rxjs';
 import {environment} from "../../../environments/environment";
 
 @Injectable()
 export class AppHttpInterceptor implements HttpInterceptor {
 
+  private cache = new Map<string, HttpResponse<any>>();
+
   constructor() {
   }
 
   intercept(httpRequest: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    //any alteration in httpRequest can be done
 
     const headers = httpRequest.headers.set('Version', environment.api.version)
       .set('Agent', environment.api.agent);
     const requestClone = httpRequest.clone({headers});
 
+    const cachedResponse = this.cache.get(httpRequest.url);
+    if (cachedResponse) {
+      return of(cachedResponse);
+    }
+
+
     return next.handle(requestClone).pipe(
+      tap(event => {
+        if (event instanceof HttpResponse) {
+          this.cache.set(httpRequest.url, event);
+        }
+      }),
       catchError((error: HttpErrorResponse) => {
         return throwError(error);
       })
